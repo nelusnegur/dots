65c97ede1877faec9028b79490531f2609ce3f4b
Nelu Snegur <nelu.snegur@pulsarplanet.com>
2021-02-06 19:19:51 +0100

    Add set_up_shell function

--- setup.sh
+++ setup.sh
@@ -19,9 +19,33 @@ function set_up_font() {
 	brew install --cask font-iosevka
 }
 
+function set_up_zshell() {
+	echo "Setting up shell..."
+	echo "Installing zsh..."
+
+	# Installing oh-my-zsh (https://ohmyz.sh/)
+	sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+	cp -a shell/zsh/themes/. ~/.oh-my-zsh/custom/themes
+}
+
+function set_up_tmux() {
+	echo "Setting up tmux..."
+	brew install tmux
+
+	cp -a shell/tmux/. ~/
+}
+
+function set_up_shell() {
+	set_up_zshell()
+	set_up_tmux()
+}
+
 
 echo "Setting up the environment..."
+
 set_up_vim()
 set_up_font()
-echo "Done!"
+set_up_shell()
 
+echo "Done!"
