88adad6197d10cccdddeeef48dc815e5930b00c8
Nelu Snegur <nelu.snegur@pulsarplanet.com>
2022-07-01 17:36:35 +0200

    Add nvim-metals

---
+++ editor/nvim/lua/usr/lsp/scala-metals.lua
@@ -0,0 +1,27 @@
+local metals = require("metals")
+local metals_config = metals.bare_config()
+
+metals_config.settings = {
+  showImplicitArguments = true,
+  showImplicitConversionsAndClasses = true,
+  showInferredType = true,
+  excludedPackages = {}
+}
+
+local handlers = require("usr.lsp.handlers")
+
+metals_config.on_attach = function (client, bufnr)
+ handlers.on_attach(client, bufnr)
+end
+
+metals_config.capabilities = handlers.capabilities
+
+
+local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "scala", "sbt" },
+  callback = function()
+    metals.initialize_or_attach(metals_config)
+  end,
+  group = nvim_metals_group
+})
--- editor/nvim/lua/usr/plugins.lua
+++ editor/nvim/lua/usr/plugins.lua
@@ -100,6 +100,9 @@ return packer.startup(function(use)
   -- Rust tools
   use "simrat39/rust-tools.nvim"
 
+  -- Scala metals
+  use "scalameta/nvim-metals"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
