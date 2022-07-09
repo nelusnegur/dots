b662db201aff670a008ba3e6a9fff4897ddcdd74
Nelu Snegur <nelu.snegur@pulsarplanet.com>
2022-07-09 17:22:53 +0200

    Enable autoformatting for Rust files on write

--- nvim/.config/nvim/lua/usr/lsp/rust-tools.lua
+++ nvim/.config/nvim/lua/usr/lsp/rust-tools.lua
@@ -1,1 +1,11 @@
 require('rust-tools').setup({})
+
+-- Autoformat on write
+vim.api.nvim_create_autocmd(
+  "BufWritePre", {
+    pattern = { "*.rs" },
+    callback = function()
+      vim.lsp.buf.formatting_sync(nil, 200)
+    end
+  }
+)
