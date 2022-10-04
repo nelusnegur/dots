e2db9d7ba2eb82aae0f53b52dde4fc784b297ed0
Nelu Snegur <nelu.snegur@pulsarplanet.com>
2022-10-04 11:57:31 +0200

    Replace deprecated formatting API

--- nvim/.config/nvim/lua/usr/lsp/autocmds.lua
+++ nvim/.config/nvim/lua/usr/lsp/autocmds.lua
@@ -3,7 +3,7 @@ vim.api.nvim_create_autocmd(
   "BufWritePre", {
     pattern = { "*.rs", "*.scala" },
     callback = function()
-      vim.lsp.buf.formatting_sync(nil, 200)
+      vim.lsp.buf.format({ async = false, timeout_ms = 200 })
     end
   }
 )
--- nvim/.config/nvim/lua/usr/lsp/handlers.lua
+++ nvim/.config/nvim/lua/usr/lsp/handlers.lua
@@ -70,7 +70,7 @@ local function lsp_keymaps(bufnr)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "[d", '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "]d", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>fo', "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>fo', "<cmd>lua vim.lsp.buf.format { async = true }<CR>", opts)
 end
 
 M.on_attach = function(client, bufnr)
@@ -84,4 +84,3 @@ local cmp_nvim_lsp = require("cmp_nvim_lsp")
 M.capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
 
 return M
-
