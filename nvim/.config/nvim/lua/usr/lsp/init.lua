89922d165cbd3e1f87e4bf8753215ac76371ecf1
Nelu Snegur <nelu.snegur@pulsarplanet.com>
2022-10-04 11:27:55 +0200

    Remove lsp_lines in favor of normal virtual_text

--- nvim/.config/nvim/lua/usr/lsp/init.lua
+++ nvim/.config/nvim/lua/usr/lsp/init.lua
@@ -1,7 +1,6 @@
 require("lspconfig")
 require("usr.lsp.handlers").setup()
 require("usr.lsp.lsp-installer")
-require("usr.lsp.lsp-lines")
 require("usr.lsp.autocmds")
 require("usr.lsp.rust-tools")
 require("usr.lsp.scala-metals")
