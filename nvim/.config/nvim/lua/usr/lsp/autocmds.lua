ad3acd396a87203d163a202cf3ffcdfc496a0783
Nelu Snegur <nelu.snegur@pulsarplanet.com>
2022-10-09 12:04:36 +0200

    Enable autoformatting for Lua

--- nvim/.config/nvim/lua/usr/lsp/autocmds.lua
+++ nvim/.config/nvim/lua/usr/lsp/autocmds.lua
@@ -1,7 +1,8 @@
 -- Autoformat on write
 vim.api.nvim_create_autocmd(
-  "BufWritePre", {
-    pattern = { "*.rs", "*.scala" },
+  "BufWritePre",
+  {
+    pattern = { "*.rs", "*.lua", "*.scala" },
     callback = function()
       vim.lsp.buf.format({ async = false, timeout_ms = 200 })
     end
