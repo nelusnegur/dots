4532a9342edf21b42bdabf089e91168299d03a0c
Nelu Snegur <nelu.snegur@pulsarplanet.com>
2022-11-06 14:33:19 +0100

    Add inlayHints settings for rust-analyzer

--- nvim/.config/nvim/lua/usr/lsp/settings/rust-analyzer.lua
+++ nvim/.config/nvim/lua/usr/lsp/settings/rust-analyzer.lua
@@ -1,9 +1,41 @@
 return {
-  -- see https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#rust_analyzer
+  -- see https://github.com/rust-lang/rust-analyzer/blob/master/docs/user/generated_config.adoc
   settings = {
     ["rust-analyzer"] = {
       lens = {
         enable = true,
+      },
+      inlayHints = {
+        lifetimeElisionHints = {
+          enable = "always",
+          useParameterNames = false
+        },
+        reborrowHints = {
+          enable = "always"
+        },
+        bindingModeHints = {
+          enable = false,
+        },
+        chainingHints = {
+          enable = false,
+        },
+        closingBraceHints = {
+          enable = false,
+          minLines = 25
+        },
+        closureReturnTypeHints = {
+          enable = "never",
+          maxLength = 25
+        },
+        parameterHints = {
+          enable = false
+        },
+        renderColons = true,
+        typeHints = {
+          enable = false,
+          hideClosureInitialization = false,
+          hideNamedConstructor = false
+        },
       },
       checkOnSave = {
         command = "clippy",
