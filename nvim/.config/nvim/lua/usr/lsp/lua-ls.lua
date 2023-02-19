af5616fe0bb7f8dfedc5c7ef9f4bff260dcbd8b8
Nelu Snegur <nelu.snegur@pulsarplanet.com>
2023-02-19 11:42:43 +0100

    Use lua_ls instead of the deprecated sumneko_lua server

---
+++ nvim/.config/nvim/lua/usr/lsp/lua-ls.lua
@@ -0,0 +1,18 @@
+require("lspconfig").lua_ls.setup {
+  settings = {
+    Lua = {
+      runtime = {
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        globals = { 'vim' },
+      },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
