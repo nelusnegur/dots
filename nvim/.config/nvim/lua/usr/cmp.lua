c5c8473c401176da8ef78cb7dde8609835f5dd5c
Nelu Snegur <nelu.snegur@pulsarplanet.com>
2023-05-18 11:07:43 +0200

    Fix formatting in cmp.lua

--- nvim/.config/nvim/lua/usr/cmp.lua
+++ nvim/.config/nvim/lua/usr/cmp.lua
@@ -45,7 +45,7 @@ cmp.setup {
   },
   mapping = {
     ["<C-p>"] = cmp.mapping.select_prev_item(),
-		["<C-n>"] = cmp.mapping.select_next_item(),
+    ["<C-n>"] = cmp.mapping.select_next_item(),
     ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
     ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
     ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
