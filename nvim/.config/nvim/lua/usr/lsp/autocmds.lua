08590985567dc3c950771d5dadb71ec47becb31d
Nelu Snegur <nelu.snegur@pulsarplanet.com>
2023-05-21 11:11:51 +0200

    Remove inlay-hints

--- nvim/.config/nvim/lua/usr/lsp/autocmds.lua
+++ nvim/.config/nvim/lua/usr/lsp/autocmds.lua
@@ -22,16 +22,3 @@ vim.api.nvim_create_autocmd("LspAttach", {
     set_lsp_keymaps(args.buf)
   end
 })
-
-vim.api.nvim_create_augroup("LspAttach_inlayhints", {})
-vim.api.nvim_create_autocmd("LspAttach", {
-  group = "LspAttach_inlayhints",
-  callback = function(args)
-    if not (args.data and args.data.client_id) then
-      return
-    end
-
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
-    require("lsp-inlayhints").on_attach(client, args.buf)
-  end,
-})
