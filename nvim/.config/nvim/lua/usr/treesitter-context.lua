fde15925bc80103167d6724b46fcf17462eef45a
Nelu Snegur <nelu.snegur@pulsarplanet.com>
2023-10-01 18:41:29 +0200

    Upgrade the treesitter-context plugin

--- nvim/.config/nvim/lua/usr/treesitter-context.lua
+++ nvim/.config/nvim/lua/usr/treesitter-context.lua
@@ -1,39 +1,14 @@
 require('treesitter-context').setup {
-  enable = true,
+  enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
   max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+  min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+  line_numbers = true,
+  multiline_threshold = 2, -- Maximum number of lines to show for a single context
   trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-  patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
-    -- For all filetypes
-    -- Note that setting an entry here replaces all other patterns for this entry.
-    -- By setting the 'default' entry below, you can control which nodes you want to
-    -- appear in the context window.
-    default = {
-      'class',
-      'function',
-      'method',
-      -- 'for', -- These won't appear in the context
-      -- 'while',
-      -- 'if',
-      -- 'switch',
-      -- 'case',
-    },
-    -- Example for a specific filetype.
-    -- If a pattern is missing, *open a PR* so everyone can benefit.
-    --   rust = {
-    --       'impl_item',
-    --   },
-  },
-  exact_patterns = {
-    -- Example for a specific filetype with Lua patterns
-    -- Treat patterns.rust as a Lua pattern (i.e "^impl_item$" will
-    -- exactly match "impl_item" only)
-    -- rust = true,
-  },
-
-  -- [!] The options below are exposed but shouldn't require your attention,
-  --     you can safely ignore them.
-
+  mode = 'cursor',  -- Line used to calculate context. Choices: 'cursor', 'topline'
+  -- Separator between context and content. Should be a single character string, like '-'.
+  -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+  separator = nil,
   zindex = 20, -- The Z-index of the context window
-  mode = 'cursor', -- Line used to calculate context. Choices: 'cursor', 'topline'
-  separator = nil, -- Separator between context and content. Should be a single character string, like '-'.
+  on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
 }
