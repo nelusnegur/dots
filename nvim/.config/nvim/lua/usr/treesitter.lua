cb99e5ee210f4bd691f01691bbfb763f2df1e9d0
Nelu Snegur <nelu.snegur@pulsarplanet.com>
2023-10-03 18:42:36 +0200

    Upgrade nvim-treesitter plugin

--- nvim/.config/nvim/lua/usr/treesitter.lua
+++ nvim/.config/nvim/lua/usr/treesitter.lua
@@ -1,18 +1,12 @@
 require("nvim-treesitter.configs").setup {
   ensure_installed = "all",
   sync_install = false,
+  auto_install = true,
   ignore_install = { "" },
-  autopairs = {
-    enable = true,
-  },
+
   highlight = {
     enable = true,
     disable = { "" },
-    additional_vim_regex_highlighting = true,
-  },
-  indent = { enable = true, disable = { "yaml" } },
-  context_commentstring = {
-    enable = true,
-    enable_autocmd = false,
+    additional_vim_regex_highlighting = false,
   },
 }
