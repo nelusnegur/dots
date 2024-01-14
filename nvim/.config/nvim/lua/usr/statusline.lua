a7dacf3f79f9c1e4c2a8417e67a5f4b55d469ed2
Nelu Snegur <nelu.snegur@pulsarplanet.com>
2024-01-14 17:03:19 +0100

    Fix the statusline after upgrade

--- nvim/.config/nvim/lua/usr/statusline.lua
+++ nvim/.config/nvim/lua/usr/statusline.lua
@@ -10,7 +10,8 @@ require("lualine").setup({
 
 -- listen lsp-progress event and refresh lualine
 vim.api.nvim_create_augroup("lualine_augroup", { clear = true })
-vim.api.nvim_create_autocmd("User LspProgressStatusUpdated", {
-    group = "lualine_augroup",
-    callback = require("lualine").refresh,
+vim.api.nvim_create_autocmd("User", {
+  group = "lualine_augroup",
+  pattern = "LspProgressStatusUpdated",
+  callback = require("lualine").refresh,
 })
