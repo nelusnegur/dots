f7e160e45acb3e1740f64dc1233e6e8801506c7f
Nelu Snegur <nelu.snegur@pulsarplanet.com>
2024-03-21 20:46:44 +0100

    Decrease yank highlight timeout

--- nvim/.config/nvim/lua/usr/autocmds.lua
+++ nvim/.config/nvim/lua/usr/autocmds.lua
@@ -2,6 +2,6 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlight when yanking text",
   group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
   callback = function()
-    vim.highlight.on_yank()
+    vim.highlight.on_yank({ higroup = "IncSearch", timeout = 50 })
   end
 })
