ce4d515640795122bae7f55f3497d60d2bf23015
Nelu Snegur <nelu.snegur@pulsarplanet.com>
2025-08-04 09:49:15 +0200

    Turn plugin update notifications off

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -22,11 +22,10 @@ require("lazy").setup({
   spec = {
     { import = "plugins" },
   },
-  -- -- Configure any other settings here. See the documentation for more details.
-  -- -- colorscheme that will be used when installing plugins.
-  -- install = { colorscheme = { "habamax" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = {
+    enabled = true,
+    notify = false
+  },
   rocks = {
     enabled = false,
   }
