a7c6ba5189cb11beef1f95aa34f9ff9fcc49023e
Nelu Snegur <nelu.snegur@pulsarplanet.com>
2025-08-17 13:56:30 +0200

    Add DAP configuration for Scala Metals

--- nvim/.config/nvim/lua/plugins/dap.lua
+++ nvim/.config/nvim/lua/plugins/dap.lua
@@ -8,7 +8,29 @@ return {
       vim.fn.sign_define("DapBreakpoint",
         { text = "", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" })
       vim.fn.sign_define("DapStopped", { text = "", texthl = "DapStopped", linehl = "DapStopped", numhl = "DapStopped" })
-    end
+    end,
+    config = function(self, opts)
+      local dap = require("dap")
+
+      dap.configurations.scala = {
+        {
+          type = "scala",
+          request = "launch",
+          name = "Run or test",
+          metals = {
+            runType = "runOrTestFile",
+          }
+        },
+        {
+          type = "scala",
+          request = "launch",
+          name = "Test target",
+          metals = {
+            runType = "testTarget"
+          }
+        }
+      }
+    end,
   },
   {
     "rcarriga/nvim-dap-ui",
